a7fcd8a9317ee71632eb2a47f464b6ac1a4f95dc
Dnica98 <daniela.nica98@gmail.com>
2024-10-04 17:25:27 +0300

    1

---
+++ index.js
@@ -0,0 +1,91 @@
+// const num = 10
+// const num2 = 20
+
+
+// if(num == num2){
+//     console.log('pass')
+// } else{
+//     console.log('not pass')
+// }
+
+
+// if(num === num2){
+//     console.log('pass')
+// } else{
+//     console.log('not pass')
+// }
+
+
+// if(num > num2){
+//     console.log('pass')
+// } else {
+//     console.log('not pass')
+// }
+
+// if(num < num2){
+//     console.log('pass')
+// } else {
+//     console.log('not pass')
+// }
+
+
+// if(num != num2){
+//     console.log('pass')
+// } else {
+//     console.log('not pass')
+// }
+
+
+// if(num !== num2){
+//     console.log('pass')
+// } else {
+//     console.log('not pass')
+// }
+
+
+// if(num >= num2){
+//     console.log('pass')
+// } else {
+//     console.log('not pass')
+// }
+
+
+// if(num <= num2){
+//     console.log('pass')
+// } else if(num2 = num) {
+//     console.log('not pass')
+// }
+
+
+// const condition1 = false;
+// const condition2 = false;
+
+// if (condition1 || condition2){
+//     console.log('pass')
+// } else {
+//     console.log('not pass')
+// }
+
+// const Name = 'Name';
+// const Name2 = 'Not Name';
+
+// const usedName = Name || Name2;
+
+// console.log(usedName)
+
+// console.log(true && false && true)
+
+// const condition3 = false;
+
+// const ternary = condition1 ? 'option1' : condition2 ? 'option2' : condition3 ? 'option3' : 'option4';
+
+// console.log(ternary)
+
+
+const condition1 = false;
+const condition2 = false;
+const condition3 = !false;
+
+const ternary = condition1 ? 'option1' : condition2 ? 'option2' : condition3 ? 'option3' : 'option4';
+
+console.log(ternary)
